9bb4b1d453cd7b816907f1c29c690b8c3d93cec1
konnyakuimo <konnyakuimo0101@gmail.com>
2022-06-02 12:43:05 +0900

    1st

---
+++ challenge12.py
@@ -0,0 +1,13 @@
+#1
+class Apple:
+    def __init__(self,w,c,m,s):
+        self.weight=w
+        self.color=c
+        self.money=m
+        self.sweet=s
+        print("Created!")
+
+ap1=Apple(100,"red",200,True)
+print(ap1.sweet)
+
+#2
