a3c84a3002086573951a2e04dacd8d45d5ccd127
konnyakuimo <konnyakuimo0101@gmail.com>
2022-06-03 12:43:46 +0900

    2nd

--- challenge12.py
+++ challenge12.py
@@ -11,3 +11,13 @@ ap1=Apple(100,"red",200,True)
 print(ap1.sweet)
 
 #2
+import math
+class Circle:
+    def __init__(self,r):
+        self.radius=r
+        print("Created!")
+    def area(self):
+        return self.radius*self.radius*math.pi
+cir1=Circle(5)
+print(cir1.area())
+#3
